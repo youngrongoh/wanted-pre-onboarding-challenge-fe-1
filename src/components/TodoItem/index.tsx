ff456ae72abe9991b2d57ba9d8e1c2f01b344875
youngrongoh <youngrong.oh@gmail.com>
2022-08-04 23:45:50 +0900

    feat. TodoItem 컴포넌트 추가

---
+++ src/components/TodoItem/index.tsx
@@ -0,0 +1,60 @@
+import React, { useState } from 'react'
+import EditableText from '../EditableText';
+import Toggle from '../Toggle';
+
+interface ITodoItem {
+  id: string;
+  title: string;
+  content: string;
+  createdAt: string;
+  updatedAt: string;
+  remove: (id: string) => void;
+  update: (id: string, data: { title: string, content: string }) => void;
+}
+
+const TodoItem = ({ id, title, content, createdAt, updatedAt, remove, update }: ITodoItem) => {
+  const [mode, setMode] = useState<React.ComponentProps<typeof EditableText>['mode']>('normal');
+  const [todo, setTodo] = useState({ title, content });
+  const [showsDetail, setShowsDetail] = useState<boolean>(false);
+
+  const toggleDetail = () => {
+    setShowsDetail(prev => !prev);
+  }
+
+  const onChange = (field: 'title' | 'content') => (value: string) => {
+    setTodo(prev => ({ ...prev, [field]: value }));
+  };
+
+  const onEditClicked = () => {
+    if (mode === 'edit') {
+      update(id, { title: todo.title, content: todo.content });
+      setMode('normal');
+    } else {
+      setMode('edit');
+    }
+  };
+
+  const onRemoveClicked = () => {
+    remove(id);
+  };
+
+  return (
+    <div>
+      <Toggle open={showsDetail} onClick={toggleDetail} />
+      <div>
+        <EditableText mode={mode} onChange={onChange('title')}>{todo.title}</EditableText>
+        <button onClick={onRemoveClicked}>🗑</button>
+        <button onClick={onEditClicked}>✏️</button>
+      </div>
+      {showsDetail && (
+        <div>
+          <EditableText mode={mode} onChange={onChange('content')}>{todo.content}</EditableText>
+          <div>{createdAt}</div>
+          <div>{updatedAt}</div>
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default TodoItem;
