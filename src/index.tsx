8b017b7271abac47036c9bc9c4c565b9ea578bf9
youngrongoh <youngrong.oh@gmail.com>
2022-08-07 21:23:50 +0900

    feat. react-query 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import Router from './Router';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 
+const queryClient = new QueryClient();
+
 root.render(
   <React.StrictMode>
-    <Router />
+    <QueryClientProvider client={queryClient}>
+        <Router />
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
