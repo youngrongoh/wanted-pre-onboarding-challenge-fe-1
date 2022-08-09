d79ac9d2e0e6fe797e9573d426302d7112dc4c98
youngrongoh <youngrong.oh@gmail.com>
2022-08-10 01:19:57 +0900

    feat. 로그아웃 버튼 추가

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import TodoList from '../components/TodoList';
+import { useAuth } from '../hooks/useAuth';
 
 interface IMain {
   
 }
 
 const Main = (props: IMain) => {
-  
+  const auth = useAuth();
+  const navigate = useNavigate();
+  const { isSignedIn } = auth.data;
+
+  const onSignOutClicked = () => {
+    auth.signOut();
+    navigate('/auth');
+  }
+
   return (
     <div>
       <nav>
-        <Link to="/auth">로그인</Link>
+        {isSignedIn ? <button onClick={onSignOutClicked}>로그아웃</button> : <Link to="/auth">로그인</Link>}
       </nav>
       <TodoList />
     </div>
