891dd16eb214f9bd622d5a6ba59ea74438dc6a78
youngrongoh <youngrong.oh@gmail.com>
2022-08-12 01:16:01 +0900

    fix. default todo 상수 분리

--- src/components/TodoList/index.tsx
+++ src/components/TodoList/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import { Todo } from '../../api/todo';
 import useTodo from '../../hooks/useTodo';
 import TodoItem from '../TodoItem';
@@ -7,19 +7,19 @@ interface ITodoList {
   
 }
 
+const DEFAULT_TODO: Todo = {
+  title: '할 일을 입력하세요.', 
+  content: '', 
+  id: Date.now().toString(32), 
+  createdAt: new Date().toISOString(),
+  updatedAt: new Date().toISOString()
+}
+
 const TodoList = (props: ITodoList) => {
   const todo = useTodo();
 
   const onAddClicked = () => {
-    const newTodo: Todo = {
-      title: '할 일을 입력하세요.', 
-      content: '', 
-      id: Date.now().toString(32), 
-      createdAt: new Date().toISOString(),
-      updatedAt: new Date().toISOString()
-    }
-
-    todo.add(newTodo);
+    todo.add(DEFAULT_TODO);
   }
 
   const update = (id: string, data: { title: string, content: string }) => {
